1cbd9b7f915895d7ba25fe0b630d2d1771b4dbb8
JoaumVictor <94501287+JoaumVictor@users.noreply.github.com>
2025-05-20 20:11:30 -0300

    Ajusta configuração do Throttler para aumentar o tempo de espera e adiciona ThrottlerGuard como APP_GUARD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,15 @@ import { Module } from '@nestjs/common';
 import { TransactionController } from './interfaces/controllers/transaction.controller';
 import { CreateTransactionUseCase } from './application/use-cases/create-transaction.use-case';
 import { InMemoryTransactionRepository } from './infra/repositories/in-memory/in-memory-transaction.repository';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
     ThrottlerModule.forRoot({
       throttlers: [
         {
-          ttl: 60,
+          ttl: 10000,
           limit: 10,
         },
       ],
@@ -21,6 +22,10 @@ import { ThrottlerModule } from '@nestjs/throttler';
     {
       provide: 'TransactionRepository',
       useClass: InMemoryTransactionRepository,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
